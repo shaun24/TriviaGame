a8c34e9fecc6e0d3f76f1e73a9cf6370e7941c42
shazamba <Shaunshah24@gmail.com>
2018-01-20 23:21:47 -0500

    5th-up

---
+++ TriviaGame-master/trivia-assets/trivia.js
@@ -0,0 +1,120 @@
+   //constants
+   const questionDiv = document.getElementById("question");
+   const choiceOne = document.getElementById("choice-one");
+   const choiceTwo = document.getElementById("choice-two");
+   const choiceThree = document.getElementById("choice-three");
+   const choiceFour = document.getElementById("choice-four");
+   const choiceFive = document.getElementById("choice-five");
+   const scoreTracker = document.getElementById("valueTracker");
+   var showTime = document.getElementById("Timer")
+
+
+   var questions = [{
+     question: "Whose house was burned to the ground?",
+     choices: ["James May", "Jeremy Clarkson", "Richard Hammond", "An audience member", "A producer of TopGear"],
+     correctAnswer: "Jeremy Clarkson"
+   }, {
+     question: "Who tried to make a car out of mud?",
+     choices: ["James May", "Jeremy Clarkson", "Richard Hammond", "The stig", "A celebrity guest"],
+     correctAnswer: "James May"
+   }, {
+     question: "Who won Battleship?",
+     choices: ["The Stig", "The American", "James May", "Richard Hammond", "Jeremy Clarkson"],
+     correctAnswer: "Richard Hammond"
+   }, {
+     question: "Who got punched in the face?",
+     choices: ["The American", "A producer of TopGear", "Jeremy Clarkson", "Richard Hammond", "A local"],
+     correctAnswer: "A producer of TopGear"
+   }, {
+     question: "Who totaled a 1.4MM Croatian super car while subsequently injuring himself?",
+     choices: ["The stig", "James May", "A celebrity guest", "Richard Hammond", "Jeremy Clarkson"],
+     correctAnswer: "Richard Hammond"
+   }]
+   
+
+   var questionCounter = 0; //1
+   var scoreCounter = 1;  
+   var selections = [];
+   var intervalOne = setInterval(decrease, 1000);
+
+   var stopWatch = {   
+        qOne : 30,
+        qTwo : 60,
+        qThree : 90,
+        qFour : 30,
+        qFive : 30,
+    }
+
+
+   function initGame(){
+       initQuestion();
+       addEvents();
+       decrease();
+       // questionTime();
+   }
+
+
+   function decrease() {
+       var tOne = stopWatch.qOne--;
+       var tTwo = stopWatch.qTwo--;
+       var tThree = stopWatch.qThree--;
+       var intervalFirst = setInterval(tOne, 1000);
+       var intervalSecond = setInterval(tTwo, 1000);
+
+       console.log(stopWatch.qOne);
+       showTime.textContent = stopWatch.qOne;
+
+       if (stopWatch.qOne === 0) {
+        clearInterval(intervalFirst);
+        initQuestion()
+        setInterval(tTwo, 1000);
+        console.log(stopWatch.qTwo);
+        showTime.textContent = stopWatch.qTwo;
+       }
+    };
+
+    function questionTimer (timer) {
+        decrease()
+
+
+    }
+
+   
+   function initQuestion() {
+       if(questionCounter < questions.length){
+           var currentQuestion = questions[questionCounter];
+           questionDiv.textContent = currentQuestion.question;
+           choiceOne.textContent = currentQuestion.choices[0];
+           choiceTwo.textContent = currentQuestion.choices[1];
+           choiceThree.textContent = currentQuestion.choices[2];
+           choiceFour.textContent = currentQuestion.choices[3];
+           choiceFive.textContent = currentQuestion.choices[4];    
+           questionCounter++;
+
+       }else {
+           // document.write(scoreCounter);
+       } 
+   }
+
+   function addEvents(){
+       var buttons = document.querySelectorAll(".answer-choice");
+       for (var i = 0; i < buttons.length; i++) {
+           buttons[i].addEventListener('click', function(e) {
+               var userChoice = this.textContent;
+               if(userChoice === questions[questionCounter - 1].correctAnswer){
+                   alert("you're correct!");
+                   scoreTracker.textContent = scoreCounter++
+               }else {
+                   alert("not correct");
+               }
+               console.log(questionCounter);
+               initQuestion();
+           });
+       }
+   }
+
+   initGame();
+
+
+
+  
